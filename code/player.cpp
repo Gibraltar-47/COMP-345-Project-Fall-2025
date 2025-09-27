f3f1036acd46051259d4770694ff4dbd85ada96e
neazizi <ngynazz@gmail.com>
2025-09-27 17:38:53 -0400

    Implement Player class with Part 2 features: territories, hand, orders

--- code/player.cpp
+++ code/player.cpp
@@ -3,6 +3,47 @@
 //
 
 #include "player.h"
-class Player {
-int id;
+#include <iostream>
+
+Player::Player(){
+    name = "Unknown";
+   territories=std::vector<Territory*>();
+    hand=std::vector<Card*>();
+    orders=std::vector<Order*>();
+}
+Player::Player(const std:: string& playerName){
+   name=playerName;
+
+}
+ std::string Player::getName() const{
+  return name;
+}
+void Player::setName(const std::string& newName){
+   name=newName;
+}
+   void Player::addTerritory(Territory* tr) {
+    if (tr != nullptr) {
+        territories.push_back(tr);
+    }
+}
+   void Player::addCard(Card* cr) {
+   if(cr!=nullptr){
+  hand.push_back(cr);
+ }
+}
+    void Player::addOrder(Order* ord) {
+    if(ord!=nullptr){
+     orders.push_back(ord);
+      }
+}
+ std::vector<Territory*> Player::toDefend(){
+    return territories;
+}
+ std::vector<Territory*> Player::toAttack(){
+ std::vector<Territory*> attackList;
+  return attackList;
+}
+ void Player::issueOrder(){
+  Order* newOrder=new Order();
+  orders.push_back(newOrder);
 }
--- code/player.h
+++ code/player.h
@@ -4,9 +4,31 @@
 
 #ifndef COMP_335_PROJECT_FALL_2025_PLAYER_H
 #define COMP_335_PROJECT_FALL_2025_PLAYER_H
+#include <string>
+#include <vector>
 
+class Territory;
+class Card;
+class Order;
+class Player {
+private:
+   std::string name;
+   std::vector<Territory*> territories;
+   std::vector<Card*> hand;
+   std::vector<Order*> orders;
+ // constructors, getters, setters
+public:
+   Player();
+   Player(const std::string& playerName);
+   std::string getName() const;
+   void setName(const std::string& newName);
+   void addTerritory(Territory* tr);
+   void addCard(Card* ca);
+   void addOrder(Order* ord);
+   std::vector<Territory*> toDefend();
+   std::vector<Territory*> toAttack();
+   void issueOrder();
 
-class player {
 };
 
 
