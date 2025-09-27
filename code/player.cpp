eceee7ed1f34a52d9c50c5f9bd6104e5f9c6d52f
neazizi <ngynazz@gmail.com>
2025-09-26 20:38:43 -0400

    creating files

---
+++ code/player.cpp
@@ -0,0 +1,8 @@
+//
+// Created by ngyna on 2025-09-26.
+//
+
+#include "player.h"
+class Player {
+int id;
+}
---
+++ code/player.h
@@ -0,0 +1,13 @@
+//
+// Created by ngyna on 2025-09-26.
+//
+
+#ifndef COMP_335_PROJECT_FALL_2025_PLAYER_H
+#define COMP_335_PROJECT_FALL_2025_PLAYER_H
+
+
+class player {
+};
+
+
+#endif //COMP_335_PROJECT_FALL_2025_PLAYER_H
