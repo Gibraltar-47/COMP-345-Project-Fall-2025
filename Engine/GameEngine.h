49303b6196ce11f09f98b7523615c3b1d831970d
Howard <haotojia@gmail.com>
2025-11-06 18:43:15 -0500

    Main game loop works for now and able to determine a winner

--- Engine/GameEngine.h
+++ Engine/GameEngine.h
@@ -2,14 +2,14 @@
 // Created by Howard on 2025-11-03.
 //
 
-#ifndef COMP_345_PROJECT_FALL_2025_PART2_GAMEENGINE_H
-#define COMP_345_PROJECT_FALL_2025_PART2_GAMEENGINE_H
+#ifndef GAMEENGINE_H
+#define GAMEENGINE_H
 
 #include <iostream>
 #include <string>
 #include <vector>
-//#include "Player.h"
-// "Map.h"
+#include "../Player/Player.h"
+#include "../part1-map/Map.h"
 
 using std::string;
 using std::cout;
@@ -21,31 +21,35 @@ using std::cin;
 class GameEngine {
     private:
         string state;   //state of the engine
-        //vector<Player*> players;
+        vector<Player*> players;
         bool gameOver;
-        //Map* map;
+        Map* map;
 
     public:
         GameEngine(); //Default constructor
+        GameEngine(const GameEngine& other);
+        GameEngine &operator=(const GameEngine &other);
         ~GameEngine();
 
-        //void setState(string& state);
+        void setState(string& state);
         string getState() const;
         void changeState(const string& newState, const string& message);
 
         void runGame();
+        friend ostream& operator<<(ostream& out, const GameEngine& engine);
 
-        //void mainGameLoop();
-        //void reinforcementPhase();
-        //void issueOrdersPhase();
-        //void executeOrdersPhase();
+        void mainGameLoop();
+        void reinforcementPhase();
+        void issueOrdersPhase(vector<Player*>& allPlayers , Map* map);
+        void executeOrdersPhase();
 
-        //bool checkWinCondition();
+        bool checkWinCondition(const std::vector<Player*>& players, Map* map);
         void waitForUser();
 
-        //void addPlayer(Player* player);
-        //void removePlayer(Player* player);
-        //void addMap(Map* map);
+        void addPlayer(Player* player);
+        void removePlayer(Player* player);
+        void addMap(Map* map);
+        void printAllPlayerOrders(const std::vector<Player*>& players);
 };
 
 
