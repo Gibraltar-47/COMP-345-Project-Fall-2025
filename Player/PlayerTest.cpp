da968b7a5a321e6226893f630d6dcc30f6901954
Howard <haotojia@gmail.com>
2025-11-20 21:15:47 -0500

    Modified the toDefend(), toAttack() and issueOrder() to call the respective PlayerStrategy version

--- Player/PlayerTest.cpp
+++ Player/PlayerTest.cpp
@@ -175,34 +175,6 @@ void Player::issueOrder(Deck& deck, int mode, Territory* sourceTerritory, int nu
 
 //returns a list of territories the player owns(to defend)
 vector<Territory*> Player::toDefend(const std::vector<Territory*>& allTerritories) const{                                                                            //Require changes to work as intended
-    /**
-    std::vector<Territory*> defendList;
-    struct Info { Territory* t; int threat; int armies; };
-
-    std::vector<Info> infos;
-    for (Territory* t : territories) {
-        if (!t) continue;
-        int threatCount = 0;
-        for (const std::string& adjName : t->getAdjTerritoriesNames()) {
-            auto it = std::find_if(allTerritories.begin(), allTerritories.end(),
-                                   [&](Territory* cand) { return cand && cand->getName() == adjName; });
-            if (it != allTerritories.end()) {
-                Territory* cand = *it;
-                if (cand->getOwner() != this) ++threatCount;
-            }
-        }
-        infos.push_back({t, threatCount, t->getNumOfArmies()});
-    }
-
-    // Sort by descending threat (border territories first), then ascending armies (weaker first)
-    std::sort(infos.begin(), infos.end(), [](const Info& a, const Info& b) {
-        if (a.threat != b.threat) return a.threat > b.threat;
-        return a.armies < b.armies;
-    });
-
-    for (auto &i : infos) defendList.push_back(i.t);
-    return defendList;
-    */
     return this->ps->toDefend(allTerritories);
 
 
@@ -210,44 +182,7 @@ vector<Territory*> Player::toDefend(const std::vector<Territory*>& allTerritorie
 
 //returns list of territories adjacent to player's territories but owned by other player(to attack)
 vector<Territory*> Player::toAttack(const std::vector<Territory*>& allTerritories) const {                              //Require changes to work as intended
-    /**
-    std::vector<Territory*> attackList;
-    // Build quick lookup of territories this player owns (by pointer and by name)
-    std::unordered_set<Territory*> ownedPtrs;
-    unordered_set<std::string> ownedNames;
-    for (Territory* t : territories) {
-        if (t) {
-            ownedPtrs.insert(t);
-            ownedNames.insert(t->getName());
-        }
-    }
 
-    for (Territory* myT : territories) {
-        if (!myT) continue;
-        for (const std::string& adjName : myT->getAdjTerritoriesNames()) {
-            auto it = std::find_if(allTerritories.begin(), allTerritories.end(),
-                                   [&](Territory* cand) { return cand && cand->getName() == adjName; });
-            if (it == allTerritories.end()) continue;
-
-            Territory* target = *it;
-            if (!target) continue;
-
-            // Skip if target is one of this player's territories (by pointer or by matching name)
-            if (ownedPtrs.count(target) || ownedNames.count(target->getName())) continue;
-
-            // avoid duplicates
-            if (std::find(attackList.begin(), attackList.end(), target) == attackList.end()) {
-                attackList.push_back(target);
-            }
-        }
-    }
-
-    // sort by ascending defending armies (easier targets first)
-    std::sort(attackList.begin(), attackList.end(),
-              [](Territory* a, Territory* b) { return a->getNumOfArmies() < b->getNumOfArmies(); });
-
-    return attackList;
-    */
     return this->ps->toAttack(allTerritories);
 }
 
