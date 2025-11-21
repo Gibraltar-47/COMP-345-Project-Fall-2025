d3d2bef082cc9e3f02f93d137d120d8323d7cc74
Howard <haotojia@gmail.com>
2025-11-20 20:51:45 -0500

    Placeholder

--- PlayerStrategy/AggressivePlayerStrategy.h
+++ PlayerStrategy/AggressivePlayerStrategy.h
@@ -10,6 +10,11 @@
 class AggressivePlayerStrategy: public PlayerStrategy {
     public:
     AggressivePlayerStrategy();
+    AggressivePlayerStrategy(Player* player): PlayerStrategy(player, StrategyType::Aggressive) {};
+
+    vector<Territory *> toAttack(const std::vector<Territory *> &allTerritories) override;
+    vector<Territory *> toDefend(const std::vector<Territory *> &allTerritories) override;
+    void issueOrder(Deck &deck, int mode, Territory *sourceTerritory, int numArmies, Territory *targetTerritory, Player &player2, Observer *obs) override;
 };
 
 
