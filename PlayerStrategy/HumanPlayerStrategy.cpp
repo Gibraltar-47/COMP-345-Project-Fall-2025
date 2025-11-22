6c4dbd39145413aa6df3c55dc0b35536eeb97191
Howard <haotojia@gmail.com>
2025-11-22 01:06:54 -0500

    placeholder

--- PlayerStrategy/HumanPlayerStrategy.cpp
+++ PlayerStrategy/HumanPlayerStrategy.cpp
@@ -13,32 +13,7 @@ void HumanPlayerStrategy::issueOrder(Deck &deck, int mode, Territory *sourceTerr
 
 
 vector<Territory*> HumanPlayerStrategy::toDefend(const std::vector<Territory *> &allTerritories) {
-    std::vector<Territory*> defendList;
-    struct Info { Territory* t; int threat; int armies; };
-
-    std::vector<Info> infos;
-    for (Territory* t : this->getPlayer()->getTerritories()) {
-        if (!t) continue;
-        int threatCount = 0;
-        for (const std::string& adjName : t->getAdjTerritoriesNames()) {
-            auto it = std::find_if(allTerritories.begin(), allTerritories.end(),
-                                   [&](Territory* cand) { return cand && cand->getName() == adjName; });
-            if (it != allTerritories.end()) {
-                Territory* cand = *it;
-                if (cand->getOwner() != this->getPlayer()) ++threatCount;
-            }
-        }
-        infos.push_back({t, threatCount, t->getNumOfArmies()});
-    }
-
-    // Sort by descending threat (border territories first), then ascending armies (weaker first)
-    std::sort(infos.begin(), infos.end(), [](const Info& a, const Info& b) {
-        if (a.threat != b.threat) return a.threat > b.threat;
-        return a.armies < b.armies;
-    });
-
-    for (auto &i : infos) defendList.push_back(i.t);
-    return defendList;
+    return this->getPlayer()->getTerritories();
 }
 
 vector<Territory*> HumanPlayerStrategy::toAttack(const std::vector<Territory *> &allTerritories) {
